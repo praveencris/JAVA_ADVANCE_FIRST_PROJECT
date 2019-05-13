c2f323f9ef4dda1c011ad4fdc7fbfe1cf0adda04
Praveen <pkchmits@gmail.com>
2019-05-13 17:26:58 +0530

    first commit

---
+++ src/firstpack/FirstServlet.java
@@ -0,0 +1,20 @@
+package firstpack;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class FirstServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+		out.println("<font color=red>Welcome to Servlets</fonts>");
+		out.println("<table> <tr>\r\n" + 
+				"    <th>Month</th>\r\n" + 
+				"    <th>Savings</th>\r\n" + 
+				"  </tr>\r\n" + 
+				"  <tr>\r\n" + 
+				"    <td>January</td>\r\n" + 
+				"    <td>$100</td>\r\n" + 
+				"  </tr></table>");
+	}
+}
